cb38c70031cd65d5f54adccf7b95545c7eadb802
zhangxueyu <zhangxueyu@qq.com>
2020-06-10 21:16:04 +0800

    liu

--- src/js/detail.js
+++ src/js/detail.js
@@ -1,7 +1,8 @@
 //获取地址栏id
 let id = location.search.substring(1).split('=')[1];
-let Datu = document.querySelector('.spic img');//大图
-let Xiaotu = document.querySelector('.list');//小图表
+let Datu = document.querySelector('#spic img');//大图
+let Datu1 = document.querySelector('#df img');//二培大图
+let Xiaotu = document.querySelector('#list');//小图表
 let btn = document.querySelector('.k-1');//购物车按钮
 let value = document.querySelector('.value');//input的值
 let jia = document.querySelector('.jia');//加号
@@ -19,24 +20,94 @@ $ajax({
     success: function (data) {
         let arrdata = JSON.parse(data);
         Datu.src = arrdata.url;
+        Datu1.src = arrdata.url;
         da.innerHTML = arrdata.title;
         xiao.innerHTML = arrdata.title2;
         mony.innerHTML = arrdata.price;
-        let url1 = arrdata.picelisturl.split(',');//获取小图数据进行渲染
-        console.log(url1);
-        let str = '<ul>';
+        let url2 = arrdata.picelisturl.split(',');//获取小图数据进行渲染
+        console.log(url2);
+        let str = '';
         console.log(str);
-        for (let i = 0; i < url1.length; i++) {//循环渲染数据
+        for (let i = 0; i < url2.length; i++) {//循环渲染数据
             str += `
-                 <li>
-                    <img src="${url1[i]}">
-                 </li>
+                
+                    <img src="${url2[i]}">
+                 
             `;
         }
-        str+='</ul>';
+        str += '';
         console.log(str);
         console.log(Xiaotu);
-        Xiaotu.innerHTML=str;
+        Xiaotu.innerHTML = str;
     }
-    
+
 })
+
+// //放大镜
+function Scale() {
+    this.scale = document.querySelector('#box1');
+    this.spic =document.querySelector('#spic'); //小图
+    this.spicimg=this.spic.children[0];
+    this.sf = document.querySelector('#sf'); //小放
+    this.df = document.querySelector('#df'); //大放
+    this.bpic = this.df.children[0]; //大图
+    this.list=document.querySelector('#list');
+    this.listimg=this.list.children;
+    console.log(this.listimg)//大盒子
+}
+
+Scale.prototype.init = function () {
+    //1.鼠标经过小图，显示小放和大放
+    this.scale.onmouseover = () => {
+        this.sf.style.display = 'block';
+        this.df.style.display = 'block';
+
+
+        //2.求小放的尺寸和比例
+        this.sf.style.width = this.spic.offsetWidth * this.df.offsetWidth / this.bpic.offsetWidth +
+        'px';
+        this.sf.style.height = this.spic.offsetHeight * this.df.offsetHeight / this.bpic.offsetHeight +
+            'px';
+
+        //比例
+        this.bili = this.bpic.offsetWidth / this.spic.offsetWidth;
+
+        //3.小图添加鼠标移动事件
+        this.spic.onmousemove = (ev) => {
+            var ev = ev || window.event;
+            let l = ev.clientX - this.scale.offsetLeft - this.sf.offsetWidth / 2;
+            let t = ev.clientY - this.scale.offsetTop - this.sf.offsetHeight / 2;
+            if (l < 0) {
+                l = 0
+            } else if (l >= this.spic.offsetWidth - this.sf.offsetWidth) {
+                l = this.spic.offsetWidth - this.sf.offsetWidth - 2;
+            }
+            if (t < 0) {
+                t = 0
+            } else if (t >= this.spic.offsetHeight - this.sf.offsetHeight) {
+                t = this.spic.offsetHeight - this.sf.offsetHeight - 2;
+            }
+
+
+            this.sf.style.left = l + 'px';
+            this.sf.style.top = t + 'px';
+            this.bpic.style.left = -l * this.bili + 'px';
+            this.bpic.style.top = -t * this.bili + 'px';
+        }
+         // 5 点击小图 切换图片
+         for(let i=0;i<this.listimg.length;i++){
+            this.listimg[i].onmousemove=()=>{
+                //console.log(this.listimg[i])
+                this.bpic.src=this.listimg[i].src;
+                this.spicimg.src=this.listimg[i].src;
+            }
+        }
+    };
+
+    this.scale.onmouseout = () => {
+        this.sf.style.display = 'none';
+        this.df.style.display = 'none';
+    };
+}
+
+new Scale().init();
--- src/js/list.js
+++ src/js/list.js
@@ -2,15 +2,15 @@ let cont = document.querySelector('.cont');
 $ajax({
     url: 'http://localhost/JS2002/SMHYSC/php/listdata.php',
     success: function (data) {
-        console.log(JSON.parse(data) )
+        console.log(JSON.parse(data))
         let arrdata = JSON.parse(data);//解析字符串
         let str = '<div class="cont-1">';
-       for(var i=0;i<arrdata.length;i++){
+        for (var i = 0; i < arrdata.length; i++) {
             str += `
             <div class="sp" style="z-index:10";>
                     <div class="sp1">
                         <a href="detail.html?=${arrdata[i].sid}">
-                        <img class="lazy" data-original="${arrdata[i].url}" width="200" height="200"/>
+                        <img class="lazy" data-original="${arrdata[i].url}" width="240" height="240"/>
                         </a>
                         <div class="yuan">
                             <img src="../img/1.tmp" alt="">
@@ -24,7 +24,7 @@ $ajax({
                         ${arrdata[i].title2}
                     </p>
                     <p class="p3">
-                        <em><b>￥</b>${arrdata[i].price}</em>
+                        <em class="price"><b>￥</b>${arrdata[i].price}</em>
                     </p>
                     <div class="shopp">
                         <ul>
@@ -44,9 +44,187 @@ $ajax({
         str += '</div>';
         cont.innerHTML = str;
         //添加懒加载
-$(function () {
-    $("img.lazy").lazyload({ effect: "fadeIn" });
-});
+        $(function () {
+            $("img.lazy").lazyload({ effect: "fadeIn" });
+        });
     }
 });
-//
+//点击展出 列表
+const dt = document.querySelectorAll('.dt');
+const dd = document.querySelectorAll('.dd');
+const d1 = document.querySelectorAll('.d1 span');
+const fenye = document.querySelector('.fenye-1');
+
+function fn() {
+    fenye.addEventListener('click', (e) => {//时间监听 点击
+        e = e || window.event;//兼容
+        var tag = e.target || src.element;//获取当前操作对象 游览器兼容
+        //console.dir(e)
+        if (tag.getAttribute('class') == 'dt') {//getAttribute() 方法返回指定属性名的属性值
+            let index = tag.getAttribute('index');
+            console.log(index);
+            if (dd[index].style.overflow == "hidden") {
+                dd[index].style.overflow = "visible";
+                d1[index].style.transform = "rotate(0deg)";
+                dd[index].style.height = "90px";
+
+            } else {
+                dd[index].style.overflow = "hidden";
+                d1[index].style.transform = "rotate(180deg)";
+                dd[index].style.height = "0";
+            }
+        }
+    })
+}
+fn()
+
+//分页结构
+!function ($) {
+    const $list = $('.cont')
+    array_default = [];//排序前的li数组
+    array = [];//排序中的数组
+    prev = null;
+    next = null;
+    //将页面的li元素加载到两个数组中
+    $('.cont').each(function (index, element) {
+        //console.log($('.cont-1 .sp'))
+        array[index] = $(this);
+        array_default[index] = $(this);
+    });
+    //渲染的外部无法获取内部的元素对象，通过事件委托实现。
+
+    //2.分页思路
+    //告知后端当前请求的是第几页数据。将当前的页面页码传递给后端(get和page)
+    $('.page').pagination({
+        pageCount: 5,//总的页数
+        jump: true,//是否开启跳转到指定的页数，布尔值。
+        coping: true,//是否开启首页和尾页，布尔值。
+        prevContent: '上一页',
+        nextContent: '下一页',
+        homePage: '首页',
+        endPage: '尾页',
+        callback: function (api) {
+            console.log(api.getCurrent());//获取的页码给后端
+            $.ajax({
+                url: 'http://localhost/JS2002/SMHYSC/php/listdata.php',
+                data: {
+                    page: api.getCurrent()
+                },
+                dataType: 'json'
+            }).done(function (data) {
+                console.log(data);
+                let $strhtml = '<div class="cont-1">';
+                $.each(data, function (index, value) {
+                    $strhtml +=
+        ` <div class="sp" style="z-index:10";>
+            <div class="sp1">
+                <a href="detail.html?=${value.sid}">
+                   <img class="lazy" src='' data-original="${value.url}" width="240" height="240"/>
+                </a>
+                <div class="yuan">
+                    <img src="../img/1.tmp" alt="">
+                </div>
+            </div>
+            <p class="p1">
+                <em></em>
+                <a href="javascript:;">${value.title}</a>
+            </p>
+            <p class="p2">
+                ${value.title2}
+            </p>
+            <p class="p3">
+                <em class="price">￥${value.price}</em>
+            </p>
+            <div class="shopp">
+                <ul>
+                    <li class="jian">–</li>
+                    <li class="m"><input type="text" value="1" class="int"></li>
+                    <li class="jia">+</li>
+                </ul>
+                
+                <a href="javascript:;" class="shopCar">
+                    <i class="icon iconfont"></i><span>加入购物车</span>
+                </a>
+        </div>
+        <div class="xiaog"></div>
+       </div>
+    `;
+                });
+                $strhtml += '</div>';
+                $list.html($strhtml);
+
+                $(function () {
+                    $("img.lazy").lazyload({ effect: "fadeIn" });
+                });
+
+                array_default = [];//排序前的li数组
+                array = [];//排序中的数组
+                prev = null;
+                next = null;
+
+                //将页面的li元素加载到两个数组中
+                $('.sp').each(function (index, element) {
+                    array[index] = $(this);
+                    array_default[index] = $(this);
+                });
+            })
+        }
+    });
+
+    //3.排序
+   // console.log($('button'))
+    $('button').eq(0).on('click', function () {
+        $.each(array_default, function (index, value) {
+            $('.cont-1').append(value);
+            console.log(value)
+        });
+        return;
+    });
+    //console.log(find('.price'))
+    $('button').eq(1).on('click', function () {
+        for (let i = 0; i < array.length - 1; i++) {
+            for (let j = 0; j < array.length - i - 1; j++) {//循环出价格数值
+                prev = parseFloat(array[j].find('.price').html().substring(1));
+                next = parseFloat(array[j + 1].find('.price').html().substring(1));
+                console.log(prev,next);
+                //通过价格的判断，改变的是li的位置。
+                if (prev > next) {
+                    let temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        //清空原来的列表，将排序后的数据添加上去。
+        //empty() : 删除匹配的元素集合中所有的子节点。
+        // $('.list ul').empty();//清空原来的列表
+        //这里能够省略empty
+        //append在追加的时候，如果追加的是jquery的元素对象，而jquery元素对象在你追加的元素中存在，直接取出存在的元素，从后面追加。
+        //如果追加的是内容结构，依然和appendChild一样，后面继续追加。
+        $.each(array, function (index, value) {
+          //  console.log(value);//n.fn.init [li, context: li]
+            $('.cont-1 ').append(value);
+        });
+    });
+    $('button').eq(2).on('click', function () {
+        for (let i = 0; i < array.length - 1; i++) {
+            for (let j = 0; j < array.length - i - 1; j++) {
+                prev = parseFloat(array[j].find('.price').html().substring(1));
+                next = parseFloat(array[j + 1].find('.price').html().substring(1));
+                //通过价格的判断，改变的是li的位置。
+                if (prev < next) {
+                    let temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    //     //清空原来的列表，将排序后的数据添加上去。
+    //     //empty() : 删除匹配的元素集合中所有的子节点。
+    //     // $('.list ul').empty();//清空原来的列表
+        $.each(array, function (index, value) {
+          //  console.log(value);
+            $('.cont-1').append(value);
+        });
+     })
+}(jQuery);
